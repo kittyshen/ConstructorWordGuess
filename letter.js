6351c57481578821aeb9ade9286933517a297098
Kitty <kittykumas@gmail.com>
2018-05-21 23:04:11 -0700

    modulized the Letter constructor outside the Word constructor and import it successfuly into the Word after refering some forum post

--- letter.js
+++ letter.js
@@ -26,6 +26,7 @@ function Letter(char){
         }
     }
 }
+module.exports = Letter;
 
 // testing letter constructor
 // var newL = new Letter("a");
@@ -33,57 +34,6 @@ function Letter(char){
 // newL.charGuesssed = true;
 // console.log(newL.hiddenChar());
 
-
-// Word.js: Contains a constructor, Word that depends on the Letter constructor. 
-//This is used to create an object representing the current word the user is attempting to guess. 
-//That means the constructor should define:
-
-// An array of new Letter objects representing the letters of the underlying word
-// A function that returns a string representing the word. This should call the function on each letter 
-//object (the first function defined in Letter.js) that displays the character or an underscore and 
-//concatenate those together.
-// A function that takes a character as an argument and calls the guess function on each letter object 
-//(the second function defined in Letter.js)
-
-
-//define Word constructor
-function Word(string){
-    this.arr = []; //initial a array of Letter obj,this need to be added otherwise can't use the push function on next line
-    for(var i =0; i<string.length ; i++){
-        this.arr.push(new Letter(string[i])); //after create Letter obj with Letter stamp push it into array
-    }
-    this.printTargetWord = function (){
-        var fullWord = [];
-        for(var i=0; i<this.arr.length ; i++){
-            fullWord += this.arr[i].char;
-        } 
-        // console.log(fullWord);
-        return fullWord;
-    };
-    this.printWord = function (){
-        var fullWord = [];
-        for(var i=0; i<this.arr.length ; i++){
-            fullWord += this.arr[i].hiddenChar();
-        } 
-        console.log(fullWord);
-        return fullWord;
-    };
-    this.woodGuess = function (char){
-        for(var i=0; i<this.arr.length ; i++){
-            this.arr[i].guess(char);
-        }
-    };
-}
-
-// testing Word constructor
-var test = new Word("cheese cake");
-console.log(test);
-console.log("*****************");
-test.printWord();
-console.log("*********after guess********");
-test.woodGuess("e");
-test.printWord();
-test.woodGuess("s");
-test.printWord();
+//having trouble modulize Letter constructor , refering this post then solved https://stackoverflow.com/questions/20534702/node-js-use-of-module-exports-as-a-constructor
 
 
---
+++ word.js
@@ -0,0 +1,52 @@
+
+// Word.js: Contains a constructor, Word that depends on the Letter constructor. 
+//This is used to create an object representing the current word the user is attempting to guess. 
+//That means the constructor should define:
+
+// An array of new Letter objects representing the letters of the underlying word
+// A function that returns a string representing the word. This should call the function on each letter 
+//object (the first function defined in Letter.js) that displays the character or an underscore and 
+//concatenate those together.
+// A function that takes a character as an argument and calls the guess function on each letter object 
+//(the second function defined in Letter.js)
+
+var Letter = require("./letter.js");
+//define Word constructor
+function Word(string){
+    this.arr = []; //initial a array of Letter obj,this need to be added otherwise can't use the push function on next line
+    for(var i =0; i<string.length ; i++){
+        this.arr.push(new Letter(string[i])); //after create Letter obj with Letter stamp push it into array
+    }
+    this.printTargetWord = function (){
+        var fullWord = [];
+        for(var i=0; i<this.arr.length ; i++){
+            fullWord += this.arr[i].char;
+        } 
+        // console.log(fullWord);
+        return fullWord;
+    };
+    this.printWord = function (){
+        var fullWord = [];
+        for(var i=0; i<this.arr.length ; i++){
+            fullWord += this.arr[i].hiddenChar();
+        } 
+        console.log(fullWord);
+        return fullWord;
+    };
+    this.woodGuess = function (char){
+        for(var i=0; i<this.arr.length ; i++){
+            this.arr[i].guess(char);
+        }
+    };
+}
+
+// testing Word constructor
+var test = new Word("cheese cake");
+console.log(test);
+console.log("*****************");
+test.printWord();
+console.log("*********after guess********");
+test.woodGuess("e");
+test.printWord();
+test.woodGuess("s");
+test.printWord();
