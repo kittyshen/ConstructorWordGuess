64692d31c6e3d729293e970f49f8baaca37a377e
Kitty <kittykumas@gmail.com>
2018-05-21 22:35:10 -0700

    build the constructor for Word

--- letter.js
+++ letter.js
@@ -10,7 +10,7 @@
 
 
 
-//define letter constructor
+//define Letter constructor
 function Letter(char){
     this.char = char;
     this.charGuesssed = false;
@@ -39,27 +39,51 @@ function Letter(char){
 //That means the constructor should define:
 
 // An array of new Letter objects representing the letters of the underlying word
-// A function that returns a string representing the word. This should call the function on each letter object (the first function defined in Letter.js) that displays the character or an underscore and concatenate those together.
-// A function that takes a character as an argument and calls the guess function on each letter object (the second function defined in Letter.js)
+// A function that returns a string representing the word. This should call the function on each letter 
+//object (the first function defined in Letter.js) that displays the character or an underscore and 
+//concatenate those together.
+// A function that takes a character as an argument and calls the guess function on each letter object 
+//(the second function defined in Letter.js)
 
+
+//define Word constructor
 function Word(string){
-    this.arr = [];
+    this.arr = []; //initial a array of Letter obj,this need to be added otherwise can't use the push function on next line
     for(var i =0; i<string.length ; i++){
-        this.arr.push(new Letter(string[i]));
+        this.arr.push(new Letter(string[i])); //after create Letter obj with Letter stamp push it into array
     }
-    this.printWord = function (){
+    this.printTargetWord = function (){
         var fullWord = [];
         for(var i=0; i<this.arr.length ; i++){
             fullWord += this.arr[i].char;
         } 
+        // console.log(fullWord);
+        return fullWord;
+    };
+    this.printWord = function (){
+        var fullWord = [];
+        for(var i=0; i<this.arr.length ; i++){
+            fullWord += this.arr[i].hiddenChar();
+        } 
         console.log(fullWord);
-    }
-    // this.woodGuess = function (char){
-        
-    // }
+        return fullWord;
+    };
+    this.woodGuess = function (char){
+        for(var i=0; i<this.arr.length ; i++){
+            this.arr[i].guess(char);
+        }
+    };
 }
 
-var test = new Word("horse");
+// testing Word constructor
+var test = new Word("cheese cake");
 console.log(test);
-console.log("*********");
+console.log("*****************");
 test.printWord();
+console.log("*********after guess********");
+test.woodGuess("e");
+test.printWord();
+test.woodGuess("s");
+test.printWord();
+
+
